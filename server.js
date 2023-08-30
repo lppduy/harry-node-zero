bb432500a39dfbe8e6050ab5e1f93f7f8336d336
lppduy <lephamphuongduy@gmail.com>
2023-08-30 21:08:37 +0700

    v17

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const express = require('express'); // commonjs
+// import express from 'express'; //es modules
 
-const app = express();
-const port = 8081;
+const app = express(); // app express
+const port = 8081; // port
 
+// khai báo route
 app.get('/', (req, res) => {
   res.send('Hello World!');
 });
 
+//
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
