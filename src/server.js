d747e1e44720c6a308f26f092072dc6f10ebf6f0
lppduy <lephamphuongduy@gmail.com>
2023-08-31 13:12:16 +0700

    v21 - static file cần macos cấp quyển cho

--- src/server.js
+++ src/server.js
@@ -13,10 +13,13 @@ const hostname = process.env.HOST_NAME;
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// config static files
+app.use(express.static(path.join(__dirname, 'public')));
+
 // khai báo route
 app.get('/', (req, res) => {
-  res.send('Hello World vs lppduy & nodemon 🚀 !');
-  // res.render('sample.ejs');
+  // res.send('Hello World vs lppduy & nodemon 🚀 !');
+  res.render('sample.ejs');
 });
 
 app.get('/abc', (req, res) => {
