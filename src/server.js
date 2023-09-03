f6b3ea15bebcc43cefd026ab4e6813bd2cc01c4b
lppduy <lephamphuongduy@gmail.com>
2023-09-03 14:44:45 +0700

    video 30 14/27p

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const express = require('express'); // commonjs
 const path = require('path'); // commonjs
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('../src/routes/web');
+const mysql = require('mysql2');
 
 const app = express(); // app express => hardcore => .uat .prod ???
 const port = process.env.PORT || 8888; // port
@@ -13,6 +14,14 @@ configViewEngine(app);
 
 // khai báo route
 app.use('/', webRoutes);
+
+// test connection
+const connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3307, // default 3306
+  user: 'root',
+  database: 'test',
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on http://${hostname}:${port}`);
