86ea192ae7fd3d8f8e5fcf4067610f550082d08d
lppduy <lephamphuongduy@gmail.com>
2023-09-04 01:24:47 +0700

    video 31

--- src/server.js
+++ src/server.js
@@ -1,9 +1,10 @@
 require('dotenv').config();
+
 const express = require('express'); // commonjs
 const path = require('path'); // commonjs
 const configViewEngine = require('./config/viewEngine');
 const webRoutes = require('../src/routes/web');
-const mysql = require('mysql2');
+const connection = require('./config/database');
 
 const app = express(); // app express => hardcore => .uat .prod ???
 const port = process.env.PORT || 8888; // port
@@ -15,19 +16,10 @@ configViewEngine(app);
 // khai báo route
 app.use('/', webRoutes);
 
-// test connection
-const connection = mysql.createConnection({
-  host: 'localhost',
-  port: 3307, // default 3306
-  user: 'root',
-  password: '123456',
-  database: 'hoidanit',
-});
-
 // simple query
 connection.query('SELECT * FROM Users', function (err, results, fields) {
-  console.log('results', results); // results contains rows returned by server
-  console.log('fields', fields); // fields contains extra meta data about results, if available
+  console.log('>>>results', results); // results contains rows returned by server
+  console.log('>>>fields', fields); // fields contains extra meta data about results, if available
 });
 
 app.listen(port, () => {
